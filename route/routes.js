47f655a46f846fb7b30946c3a0f9c6d49ba01e21
Pankaj-Aru <pankajpatilaru@gmail.com>
2022-04-23 23:57:33 +0530

    deleting old file

--- route/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { getData } from "../Controller/dataController.js";
-
-router.get("/getData", getData);
-
-export default router;
