c0e69145d32c235128d7002f76d2c150c61b6b78
Pankaj Aru <pankaj.aru@mavenir.com>
2023-01-03 11:36:33 +0530

    added new comment

--- route/routes.js
+++ route/routes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 this is the first commit
 this is the second commit
 
@@ -5,6 +6,8 @@ this is the second commit
 
 
 
+=======
+>>>>>>> parent of b844f48 (this is my first commit, into learning of git)
 import express from "express";
 const router = express.Router();
 
